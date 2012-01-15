e4eb737926caacd32da0923f6cb88624e3911ebb
jsbaes@gmail.com <jsbaes@gmail.com@cb528535-9a56-6260-1aee-55f26b59d309>
2012-01-15 18:04:37 +0000

    Limpieza de código e introducción de comentarios en formato Javadoc

--- trunk/COLEGIO_INFTEL/src/es/uma/masterinftel/colegio_inftel/modelo/dto/ProfesoresDTO.java
+++ trunk/COLEGIO_INFTEL/src/es/uma/masterinftel/colegio_inftel/modelo/dto/ProfesoresDTO.java
@@ -1,106 +1,181 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.uma.masterinftel.colegio_inftel.modelo.dto;
 
 /**
+ * Clase DTO (TransferObject) para la tabla Profesores
  *
- * @author jesus
+ * @author Jesús Barriga
+ * @version 1.0, Diciembre-2011
  */
 public class ProfesoresDTO {
 
-   private Integer id;
-   private String dni_doc;
-   private String nombre;
-   private String apellido1;
-   private String apellido2;
-   private String telfcontacto;
-   private String observaciones;
-   private String usuario;
-   private String password;
-   private String email;
-   
-   
-   public String getApellido1() {
-      return apellido1;
-   }
+    private Integer id;
+    private String dni_doc;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String telfcontacto;
+    private String observaciones;
+    private String usuario;
+    private String password;
+    private String email;
 
-   public String getApellido2() {
-      return apellido2;
-   }
+    /**
+     * Método Get
+     * @return Primer apellido
+     */
+    public String getApellido1() {
+        return apellido1;
+    }
 
-   public String getDni_doc() {
-      return dni_doc;
-   }
+    /**
+     * Método Get
+     * @return Segundo apellido
+     */
+    public String getApellido2() {
+        return apellido2;
+    }
 
-   public String getEmail() {
-      return email;
-   }
+    /**
+     * Método Get
+     * @return Dni
+     */
+    public String getDni_doc() {
+        return dni_doc;
+    }
 
-   public Integer getId() {
-      return id;
-   }
+    /**
+     * Método Get
+     * @return Dirección de eMail
+     */
+    public String getEmail() {
+        return email;
+    }
 
-   public String getNombre() {
-      return nombre;
-   }
+    /**
+     * Método Get
+     * @return Identificador
+     */
+    public Integer getId() {
+        return id;
+    }
 
-   public String getObservaciones() {
-      return observaciones;
-   }
+    /**
+     * Método Get
+     * @return Nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
 
-   public String getPassword() {
-      return password;
-   }
+    /**
+     * Método Get
+     * @return Campo observaciones
+     */
+    public String getObservaciones() {
+        return observaciones;
+    }
 
-   public String getTelfcontacto() {
-      return telfcontacto;
-   }
+    /**
+     * Método Get
+     * @return Contraseña
+     */
+    public String getPassword() {
+        return password;
+    }
 
-   public String getUsuario() {
-      return usuario;
-   }
+    /**
+     * Método Get
+     * @return Teléfono de contacto
+     */
+    public String getTelfcontacto() {
+        return telfcontacto;
+    }
 
-   public void setApellido1(String apellido1) {
-      this.apellido1 = apellido1;
-   }
+    /**
+     * Método Get
+     * @return Identificador como usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
 
-   public void setApellido2(String apellido2) {
-      this.apellido2 = apellido2;
-   }
+    /**
+     * Método Set
+     * @param apellido1 primer apellido
+     */
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
 
-   public void setDni_doc(String dni_doc) {
-      this.dni_doc = dni_doc;
-   }
+    /**
+     * Método Set
+     * @param apellido2 segundo apellido
+     */
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    /**
+     * Método Set
+     * @param dni_doc dni
+     */
+    public void setDni_doc(String dni_doc) {
+        this.dni_doc = dni_doc;
+    }
 
-   public void setId(Integer id) {
-      this.id = id;
-   }
+    /**
+     * Método Set
+     * @param email dirección de email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-   public void setNombre(String nombre) {
-      this.nombre = nombre;
-   }
+    /**
+     * Método Set
+     * @param id identificador
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-   public void setObservaciones(String observaciones) {
-      this.observaciones = observaciones;
-   }
+    /**
+     * Método Set
+     * @param id nombre nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-   public void setPassword(String password) {
-      this.password = password;
-   }
+    /**
+     * Método Set
+     * @param observaciones campo observaciones
+     */
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
 
-   public void setTelfcontacto(String telfcontacto) {
-      this.telfcontacto = telfcontacto;
-   }
+    /**
+     * Método Set
+     * @param id password contraseña
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-   public void setUsuario(String usuario) {
-      this.usuario = usuario;
-   }
+    /**
+     * Método Set
+     * @param telfcontacto teléfono de contacdto
+     */
+    public void setTelfcontacto(String telfcontacto) {
+        this.telfcontacto = telfcontacto;
+    }
 
-   
+    /**
+     * Método Set
+     * @param usuario identificador como usuario
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
 }
--- trunk/COLEGIO_INFTEL/src/es/uma/masterinftel/colegio_inftel/modelo/dto/RolProfesorDTO.java
+++ trunk/COLEGIO_INFTEL/src/es/uma/masterinftel/colegio_inftel/modelo/dto/RolProfesorDTO.java
@@ -1,32 +1,45 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.uma.masterinftel.colegio_inftel.modelo.dto;
 
 /**
+ * Clase DTO (TransferObject) para la tabla Rol_Profesor
  *
- * @author jsbaes
+ * @author Jesús Barriga
+ * @version 1.0, Diciembre-2011
  */
 public class RolProfesorDTO {
-   private Integer id_rol_fk;
-   private Integer id_profesor_fk;
 
+    private Integer id_rol_fk;
+    private Integer id_profesor_fk;
+
+    /**
+     * Método Get
+     * @return Identificador de profesor
+     */
     public Integer getId_profesor_fk() {
         return id_profesor_fk;
     }
 
+    /**
+     * Método Get
+     * @return Identificador de rol
+     */
     public Integer getId_rol_fk() {
         return id_rol_fk;
     }
 
+    /**
+     * Método Set
+     * @param id_profesor_fk identificdor de profesor
+     */
     public void setId_profesor_fk(Integer id_profesor_fk) {
         this.id_profesor_fk = id_profesor_fk;
     }
 
+    /**
+     * Método Set
+     * @param id_rol_fk identificdor de rol
+     */
     public void setId_rol_fk(Integer id_rol_fk) {
         this.id_rol_fk = id_rol_fk;
     }
-    
-    
 }
