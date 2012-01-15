d34f5e2a8f4e43bc1a39cad963661ac6c96bc66d
jsbaes@gmail.com <jsbaes@gmail.com@cb528535-9a56-6260-1aee-55f26b59d309>
2012-01-15 18:07:30 +0000

    Limpieza de código e introducción de comentarios en formato Javadoc

--- trunk/COLEGIO_INFTEL/src/es/uma/masterinftel/colegio_inftel/modelo/dao/GruposDAO.java
+++ trunk/COLEGIO_INFTEL/src/es/uma/masterinftel/colegio_inftel/modelo/dao/GruposDAO.java
@@ -11,21 +11,21 @@ import java.util.ArrayList;
  * GruposDAO es la clase DAO (Data Access Object) del modelo para el acceso  
  * a objetos GruposDTO.
  * 
- * @author Jesus Barriga
+ * @author Jesús Barriga
  * @version 1.0, Diciembre-2011
  * 
  */
 public class GruposDAO extends GenericDAO {
-    
+
     /**
      * Sentencia SQL para recuperar los grupos pertenecientes a un curso.
      */
     public static final String SQL_SELECT_GRUPOS_CURSO =
-        "SELECT A.ID, A.DESC FROM " +
-        "GRUPOS A, CURSOS_GRUPOS B " +
-        "WHERE A.ID = B.ID_GRUPO_FK " +
-        "AND B.ID_CURSO_FK=?;";
-    
+            "SELECT A.ID, A.DESC FROM "
+            + "GRUPOS A, CURSOS_GRUPOS B "
+            + "WHERE A.ID = B.ID_GRUPO_FK "
+            + "AND B.ID_CURSO_FK=?;";
+
     /**
      * Consulta de los grupos pertenecientes al curso que se pasa como parámetro
      * 
@@ -34,21 +34,21 @@ public class GruposDAO extends GenericDAO {
      * @return ArrayList de GruposDTO con los grupos recuperados
      * @throws java.sql.SQLException
      */
-    public ArrayList<GruposDTO> obtenerGruposByCurso(Connection conn, Integer idCurso) 
+    public ArrayList<GruposDTO> obtenerGruposByCurso(Connection conn, Integer idCurso)
             throws SQLException {
 
         PreparedStatement ps = null;
         ResultSet rs = null;
         ArrayList grupos = new ArrayList();
-        
+
         try {
-            if (conn!=null){
+            if (conn != null) {
                 ps = (PreparedStatement) conn.prepareStatement(SQL_SELECT_GRUPOS_CURSO);
                 ps.setInt(1, idCurso);
                 rs = ps.executeQuery();
 
                 //Creamos la lista con todos los objetos cursos
-                while (rs.next()){
+                while (rs.next()) {
                     GruposDTO dto = new GruposDTO();
                     dto.setId(rs.getInt("id"));
                     dto.setDesc(rs.getString("desc"));
